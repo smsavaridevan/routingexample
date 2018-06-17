c6a56df0c0c1d603eee5cd645346e2221a81f0af
Savari <savari.s@modefinserver.com>
2018-06-17 17:24:01 +0530

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,65 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+
+import { RouterModule} from '@angular/router';
 import { AppComponent } from './app.component';
+import { NewcompComponent } from './newcomp/newcomp.component';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+import { NavComponent } from './nav/nav.component';
+import { Page3Component } from './page3/page3.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { HomeComponent } from './home/home.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { FooterComponent } from './footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewcompComponent,
+    Page1Component,
+    Page2Component,
+    NavComponent,
+    Page3Component,
+    LoginComponent,
+    SignupComponent,
+    HomeComponent,
+    WelcomeComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+    {
+        path: '',
+        component: WelcomeComponent
+     },
+	  {
+         path: 'Page1',
+         component: Page1Component
+      },
+	  {
+         path: 'Page2',
+         component: Page2Component
+      },
+    {
+        path: 'Page3',
+        component: Page3Component
+     },
+    {
+        path: 'Login',
+        component: LoginComponent
+     },
+    {
+        path: 'Signup',
+        component: SignupComponent
+     }
+   ])
   ],
   providers: [],
   bootstrap: [AppComponent]
